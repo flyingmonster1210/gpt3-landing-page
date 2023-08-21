8f30ad4c4cb9227fd50bca6648b23169731480c3
Wayne <zweijie1210@outlook.com>
2023-08-21 13:52:40 -0700

    Create components and containers folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Article, Brand, CTA, Feature, Navbar } from './components'
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers/index'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Brand />
+      <Feature />
+      <Article />
+      <CTA />
+    </div>
   )
 }
 
